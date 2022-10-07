4784d7a7b2760e691a6ad485b95627a40d0f33ac
Manas Sarpatwar <manas.sarpatwar@kittoffices.com>
2022-10-07 21:09:17 +0100

    another test

---
+++ services/addition/main.go
@@ -0,0 +1,9 @@
+package addition
+
+import (
+	"golang.org/x/exp/constraints"
+)
+
+func calculate[N constraints.Float | constraints.Integer](a, b N) N {
+	return a + b
+}
---
+++ services/addition/main_test.go
@@ -0,0 +1,11 @@
+package addition
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddition(t *testing.T) {
+	assert.Equal(t, 6, calculate(2, 4))
+}
